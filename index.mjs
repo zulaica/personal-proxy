bdc7261a80191ae56d1ce88333f5999fe05f8ea2
David Zulaica <zulaica@gmail.com>
2022-06-13 14:48:54 -0700

    Prefer destructuring assignment

--- index.mjs
+++ index.mjs
@@ -2,13 +2,11 @@ import server from "./modules/server.mjs";
 import CONFIG from "./modules/config.mjs";
 
 server.listen(CONFIG.port, CONFIG.ipAddress, () => {
-  const serverAddress = server.address();
+  const { address, port } = server.address();
 
   console.clear();
   console.log("Starting proxy server.");
-  console.log(
-    `Serving on http://${serverAddress.address}:${serverAddress.port}/`
-  );
+  console.log(`Serving on http://${address}:${port}/`);
   console.info("Type CTRL-C to stop the proxy server.");
   console.log("");
 });
