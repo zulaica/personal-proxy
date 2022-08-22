f6c4d8359b3393334b2f1181e332e460dc0f1a41
David Zulaica <zulaica@gmail.com>
2022-08-22 00:09:48 -0700

    Update allowed origin to check against list of URLs

--- modules/loadEnvVars.mjs
+++ modules/loadEnvVars.mjs
@@ -2,7 +2,7 @@ import ENV_VARS from './envVars.mjs';
 
 const loadEnvVars = () =>
   Object.assign(process.env, {
-    ACCESS_DOMAIN: ENV_VARS.accessDomain,
+    ALLOWED_DOMAINS: ENV_VARS.allowedDomains,
     INSTAGRAM_ACCESS_TOKEN: ENV_VARS.instagram.accessToken,
     INSTAGRAM_FIELDS: ENV_VARS.instagram.fields,
     INSTAGRAM_USER_ID: ENV_VARS.instagram.userId,
